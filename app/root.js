6535989945afab170948e63e5b6aba5a2a7f2b98
mit_nick <934858124@qq.com>
2017-12-12 23:41:10 +0800

    a

---
+++ app/root.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import Header from './components/header'
+import Player from './page/player'
+import MusicList from './page/musicList'
+import { MUSIC_LIST } from './config/musiclist'
+import { Router,IndexRoute,Link,Route,hashHistory } from 'react-router'
+import Pubsub from 'pubsub-js'
+
+let App=React.createClass({
+    //始化组件的状态
+    getInitialState(){
+        return{
+            musicList:MUSIC_LIST,
+            currentMusicItem:MUSIC_LIST[0]
+        }
+    },
+    playMusic(musicItem){
+        $("#player").jPlayer('setMedia',{
+            mp3:musicItem.file
+        }).jPlayer('play');
+        this.setState({
+            currentMusicItem:musicItem
+        })
+    },
+    playNext(type="next"){
+        let index=this.findMusicIndex(this.state.currentMusicItem);
+        let newIndex=null;
+        let musicListLength=this.state.musicList.length;
+        if(type==='next'){
+            newIndex=(index+1)%musicListLength;
+        }else{
+            newIndex=(index-1+musicListLength)%musicListLength;
+        }
+        this.playMusic(this.state.musicList[newIndex]);
+    },
+    findMusicIndex(musicItem){
+        return this.state.musicList.indexOf(musicItem);
+    },
+    //通知组件已经加载完成
+    componentDidMount(){
+        $("#player").jPlayer({
+            // ready:function(){
+            //     $(this).jPlayer('setMedia',{
+            //         // mp3:'http://tingwa.oss-cn-shanghai.aliyuncs.com/2017-01/05/20170105123005-MzkzMTcz.mp3?OSSAccessKeyId=3b1nzo7roav1h50rcp0a35nw&Expires=1511506386&Signature=WmcqXQ2v4wIMjuK1pOlvCwScLZ4%3D'
+            //         mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+            //     }).jPlayer('play');
+            // },
+            supplied:'mp3',
+            vmode:'window'
+        });
+        this.playMusic(this.state.currentMusicItem);
+        $("#player").bind($.jPlayer.event.ended,(e) => {
+            this.playNext();
+        });
+        Pubsub.subscribe('DELETE_MUSIC',(msg,musicItem) => {
+            this.setState({
+                musicList:this.state.musicList.filter(item => {
+                    return item !== musicItem;
+                })
+            });
+            if(this.state.musicList.length===0){
+                $("#player").jPlayer('pause');
+                $("#root").css('display','none');
+            }else if(this.state.currentMusicItem === musicItem){
+                this.playNext('next');
+            }
+        });
+        Pubsub.subscribe('PLAY_MUSIC',(msg,musicItem) => {
+            this.playMusic(musicItem);
+        });
+        Pubsub.subscribe('PLAY_PREV',(msg,musicItem) => {
+            this.playNext('prev');
+        });
+        Pubsub.subscribe('PLAY_NEXT',(msg,musicItem) => {
+            this.playNext();
+        });
+    },
+    componentWillUnmount(){
+        Pubsub.unsubscribe('PLAY_MUSIC');
+        Pubsub.unsubscribe('DELETE_MUSIC');
+        Pubsub.unsubscribe('PLAY_PREV');
+        Pubsub.unsubscribe('PLAY_NEXT');
+        $("#player").unbind($.jPlayer.event.ended);
+    },
+    progressChangeHandler(progress){
+    },
+    render(){
+        return (
+            <div>
+                <Header />
+                {React.cloneElement(this.props.children,this.state)}
+                {/*{this.props.children}*/}
+                {/*<Player currentMusicItem={this.state.currentMusicItem}>*/}
+
+                {/*</Player>*/}
+                {/*<MusicList currentMusicItem={this.state.currentMusicItem} musicList={this.state.musicList}>*/}
+
+                {/*</MusicList>*/}
+            </div>
+        )
+    }
+});
+
+let Root=React.createClass({
+    render(){
+        return(
+            <Router history={hashHistory}>
+                <Route path="/" component={App}>
+                    <IndexRoute component={Player}></IndexRoute>
+                    <Route path="/list" component={MusicList}></Route>
+                </Route>
+            </Router>
+        )
+    }
+});
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.tpl.html',
+            inject: 'body',
+            filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
+};
